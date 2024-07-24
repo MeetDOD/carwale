ded79525162685e85407cb2b5c5a17ab3186f996
meet <dodiyameet041@gmail.com>
2024-07-25 03:07:39 +0530

    Admin auth added for create car

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.get('/getAll-car',getAllCar);
 router.get('/getCarById-car/:slug',getCarById);
-router.post('/create-car',upload.array('productPictures',50),createCar);
+router.post('/create-car',requireLogin,isAdmin,upload.array('productPictures',50),createCar);
 router.put('/update-car/:pid',requireLogin,isAdmin,formidable(),updatecar);
 router.delete('/delete-car/:pid',requireLogin,isAdmin,deleteCar);
 router.get('/related-car/:cid/:bid',relatedCar)
